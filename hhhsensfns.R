c629303e690a4a3c23201d4b11763a029ba6a9dc
Jake Bowers <jake@jakebowers.org>
2018-08-15 09:14:56 -0400

    Adding HHH sensitivity functions with my edits

---
+++ hhhsensfns.R
@@ -0,0 +1,7 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>301 Moved Permanently</title>
+<script src="/cdn-cgi/apps/head/04k0agdUNBFwNntFxB6xmmt5lZY.js"></script></head><body>
+<h1>Moved Permanently</h1>
+<p>The document has moved <a href="http://www.jakebowers.org/ICPSR/hhhsensfns.R">here</a>.</p>
+</body></html>
